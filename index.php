0513fa57719ec5fa6dd30f7028c34af2a0399f1c
Tommy Vercety <donvercety@users.noreply.github.com>
2015-02-11 10:33:02 +0200

    init

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+	
+include('Validate.php');
+$v = new Validate();
+
+$valid = $v->check($_POST, [
+	'username' => ['required' => TRUE, 'min' => 6, 'alpha_dash' => TRUE],
+	'email   ' => ['required' => TRUE, 'valid_email' => TRUE],
+	'password' => ['required' => TRUE, 'min' => 8,],
+]);
+
+var_dump($valid->errors());
+var_dump($valid->passed());
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Validate</title>
+</head>
+<body>
+	<form method="POST" action="">
+		<input type="text" name="username" placeholder="username"/>
+		<input type="text" name="email" placeholder="email"/>
+		<input type="password" name="password" placeholder="password"/>
+		<input type="submit" value="Send">
+	</form>
+</body>
+</html>
